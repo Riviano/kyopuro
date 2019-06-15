e68bca3897f0e549425856e9feb85450bdc3dbc6
mizuiro_rivi <onodaha@gmail.com>
2019-06-15 15:48:32 +0900

    ファイルの更新

--- 20190526proC.cpp
+++ 20190526proC.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	int N, M; cin >> N >> M;
 
-	vector<vector<int>> vec(M);
+	vector<vector<int>> vec(M);//動的配列
 
 	for (int i = 0; i < M; ++i) {
 		int k; cin >> k;
